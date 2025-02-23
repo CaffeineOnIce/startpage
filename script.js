8379516020fe19659f76dd32511aec3a692af9ca
CaffeineOnIce <samanwaya.dutta@gmail.com>
2025-02-23 15:46:11 +0530

    UI changes

--- script.js
+++ script.js
@@ -47,8 +47,12 @@ function setSlide(n) {
 
 function updateClock() {
 	const now = new Date();
-	const day = now.toLocaleDateString('en-GB', { weekday: 'long' }).charAt(0).toUpperCase() + now.toLocaleDateString('en-GB', { weekday: 'long' }).slice(1).toLowerCase();
-	const dateString = `${day} ${now.toLocaleDateString()} ${now.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' })}`;
+	const day = now.toLocaleDateString('en-GB', { weekday: 'long' });
+	const dayOfMonth = String(now.getDate()).padStart(2, '0');
+	const month = String(now.getMonth() + 1).padStart(2, '0');
+	const year = now.getFullYear();
+	const time = now.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+	const dateString = `${day.charAt(0).toUpperCase() + day.slice(1).toLowerCase()} ${dayOfMonth}-${month}-${year} ${time}`;
 	document.getElementById('codes').textContent = dateString;
 	setTimeout(updateClock, 1000);
 }
